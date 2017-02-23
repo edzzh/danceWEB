a5bb0ea77aa673c4939d28a53301ad04eeed8070
edzzh <edgarhart@inbox.lv>
2017-02-23 03:12:45 +0200

    Izlaboju dažus bug's ar navigāciju, izveidoju paša javascript failu main.js public mapē un pievienoju wow.js priekš fadeIn opcijas

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,11 +6,27 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/jquery/dist/jquery.js');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js')
+  app.import('bower_components/bootstrap/js/dropdown.js');
+  app.import('bower_components/bootstrap/js/alert.js');
+  app.import('bower_components/bootstrap/js/affix.js');
+  app.import('bower_components/bootstrap/js/transition.js');
+  app.import('bower_components/bootstrap/js/tooltip.js');
+  app.import('bower_components/bootstrap/js/tab.js');
+  app.import('bower_components/bootstrap/js/scrollspy.js');
+  app.import('bower_components/bootstrap/js/popover.js');
+  app.import('bower_components/bootstrap/js/modal.js');
+  app.import('bower_components/bootstrap/js/collapse.js');
+  app.import('bower_components/bootstrap/js/carousel.js');
+  app.import('bower_components/bootstrap/js/button.js');
+  app.import('bower_components/wow/dist/wow.js');
+  app.import('bower_components/wow/css/libs/animate.css');
+
 
   return app.toTree();
 };
---
+++ public/assets/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //init wow js
+    wow = new WOW({
+      animateClass: 'animated',
+      offset: 100
+    });
+    wow.init();
+
+    
+});
