d40c5bb3bb4fd791e2b99d2bf03c6bdfd1ec5545
edzzh <edgarhart@inbox.lv>
2017-03-10 14:45:35 +0200

    Aizpildiju About Us sadalju un Contact sadalju, ir probleemas ar onload window funckiju in main.js

--- public/assets/main.js
+++ public/assets/main.js
@@ -6,5 +6,28 @@ $(document).ready(function(){
     });
     wow.init();
 
-    
+    //configured carousel interval time
+     $("#myCarousel").carousel({
+         interval : 2000,
+         pause: false
+     });
+
+    //contact form validations
+     $('#characterLeft').text('500 characters left');
+     $('#message').keydown(function () {
+       var max = 500;
+       var len = $(this).val().length;
+       if (len >= max) {
+           $('#characterLeft').text('You have reached the limit');
+           $('#characterLeft').addClass('red');
+           $('#btnSubmit').addClass('disabled');
+       }
+       else {
+           var ch = max - len;
+           $('#characterLeft').text(ch + ' characters left');
+           $('#btnSubmit').removeClass('disabled');
+           $('#characterLeft').removeClass('red');
+       }
+     });
+
 });
